7ca191e5f5b2d1ede2f01c48c20b63d2ec50602a
은지 <yeochinlebelomgeole@gmail.com>
2024-01-25 11:53:03 +0900

    🌧 :: 동아리 회식 관리 페이지 퍼블리싱

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Mainpage from "./page/Mainpage";
 import { GlobalStyle } from "./GlobalStyle.style";
 import Applicationpage from "./page/Applicationpage";
+import Dinepage from "./page/Dinepage";
 
 function MainRouter() {
   return (
@@ -10,6 +11,7 @@ function MainRouter() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/Application" element={<Applicationpage />} />
+        <Route path="/Dine" element={<Dinepage />} />
       </Routes>
     </BrowserRouter>
   );
