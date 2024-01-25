9152864a7325deaad4e701b2223e21ba81d1b780
은지 <wej0513@dsm.hs.kr>
2024-01-25 13:10:39 +0900

    MainRouter

--- src/MainRouter.tsx
+++ src/MainRouter.tsx
@@ -2,7 +2,8 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Mainpage from "./page/Mainpage";
 import { GlobalStyle } from "./GlobalStyle.style";
 import Applicationpage from "./page/Applicationpage";
-
+import Dinepage from "./page/Dinepage";
+import ApplicantMgtpage from "./page/ApplicantMgtpage";
 function MainRouter() {
   return (
     <BrowserRouter>
@@ -10,6 +11,8 @@ function MainRouter() {
       <Routes>
         <Route path="/" element={<Mainpage />} />
         <Route path="/Application" element={<Applicationpage />} />
+        <Route path="/Dine" element={<Dinepage />} />
+        <Route path="/ApplicantMgt" element={<ApplicantMgtpage />} />
       </Routes>
     </BrowserRouter>
   );
